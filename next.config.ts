1616f8a624e5cb23b09dc1ac544538a8f2f56ea5
Magdy Atef Zahran <magdenoooo@gmail.com>
2025-09-03 12:05:35 +0300

    إصلاح تكوين next/image للـ localhost

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/uploads/**',
+      },
     ],
   },
   async rewrites() {
